b00941d4be21360a71e7e0056cc791025ddc68e4
36dgrs <36degrees@mail.ru>
2022-08-24 20:07:41 +0300

    aleksey andreev lesson 3

--- lib/participants/andreev_aleksei/lessons/lesson_3/andreev_aleksei_lesson_3_page.dart
+++ lib/participants/andreev_aleksei/lessons/lesson_3/andreev_aleksei_lesson_3_page.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'keyboard.dart';
+
 class AndreevAlekseiLesson3Page extends StatefulWidget {
   const AndreevAlekseiLesson3Page({
     Key? key,
@@ -33,146 +35,25 @@ class _AndreevAlekseiLesson3PageState extends State<AndreevAlekseiLesson3Page> {
             ),
           ),
         ),
-        Expanded(
-          child: GridView(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-            ),
-            children: [
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '7');
-                  });
-                },
-                child: const Text(
-                  '7',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '8');
-                  });
-                },
-                child: const Text(
-                  '8',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '9');
-                  });
-                },
-                child: const Text(
-                  '9',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '4');
-                  });
-                },
-                child: const Text(
-                  '4',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '5');
-                  });
-                },
-                child: const Text(
-                  '5',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '6');
-                  });
-                },
-                child: const Text(
-                  '6',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '1');
-                  });
-                },
-                child: const Text(
-                  '1',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '2');
-                  });
-                },
-                child: const Text(
-                  '2',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '3');
-                  });
-                },
-                child: const Text(
-                  '3',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = '';
-                  });
-                },
-                child: const Text(
-                  'C',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    displayText = addingNewDigit(displayText, '0');
-                  });
-                },
-                child: const Text(
-                  '0',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ),
-            ],
-          ),
+        Keyboard(
+          onClean: () {
+            setState(() {
+              displayText = '';
+            });
+          },
+          valueChangedOnTap: (val) {
+            setState(() {
+              if (displayText.length <= 9) {
+                displayText = displayText + val;
+              }
+              else {
+                return;
+              }
+            });
+          },
+
         ),
       ]),
     ));
-  }
-}
-
-String addingNewDigit(String displayingNumber, String newDigit) {
-  if (displayingNumber.length <= 9) {
-    final String result = '$displayingNumber$newDigit';
-    return result;
-  } else {
-    return displayingNumber;
   }
 }
