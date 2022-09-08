e8550ef18a560145422d76079a94bf18af767d1c
agumirovz <agumirovz@gmail.com>
2022-09-08 21:09:54 +0600

    askhat gumirov lesson 2

--- lib/participants/askhat_gumirov/lessons/lesson_2/main.dart
+++ lib/participants/askhat_gumirov/lessons/lesson_2/main.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter_learning/participants/askhat_gumirov/lessons/lesson_2/RowWidget.dart';
 
 void main() {
   runApp(const Main());
 }
+
+final columnList = <Widget>[];
+
+late double _maxWidth;
 
 class Main extends StatelessWidget {
   const Main({super.key});
@@ -11,130 +15,102 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: LayoutBuilder(
-        builder: (context, constraints) {
-          final maxWidth = constraints.biggest.width * 0.25;
-          final maxHeight = constraints.maxHeight;
-          return Row(
-            children: [
-              Container(
-                width: maxWidth,
-                color: Colors.blue,
-                child: Column(
-                  children: [
-                    Placeholder(
-                      fallbackHeight: maxHeight * 0.2,
-                    ),
-                    SizedBox(
-                      height: maxHeight * 0.2,
-                      width: double.infinity,
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwY2F0fGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-                      ),
-                    ),
-                    SizedBox(
-                      height: maxHeight * 0.2,
-                      width: double.infinity,
-                      child: const Text('TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT'),
-                    ),
-                    SizedBox(
-                      height: maxHeight * 0.2,
-                      width: double.infinity,
-                      child: TextButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.orange),
-                          padding: MaterialStateProperty.all(const EdgeInsets.all(5)),
-                        ),
-                        onPressed: () {
-                          return;
-                        },
-                        child: const Text('BUTTON'),
-                      ),
-                    ),
-                    SizedBox(
-                      height: maxHeight * 0.2,
-                      width: double.infinity,
-                      child: ListView(
-                        children: const [
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                          Center(child: Text('ListView')),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+      home: Material(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            _maxWidth = constraints.biggest.width * 0.25;
+            final list = ListOfWidgets();
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: list.fillColumn(),
               ),
-              SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(vertical: 18),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: maxWidth,
-                          child: const Icon(
-                            Icons.home,
-                            size: 50,
-                            color: Colors.pink,
-                          ),
-                        ),
-                        SizedBox(
-                          width: maxWidth,
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(width: 2, color: Colors.blue),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: maxWidth,
-                          child: const ColoredBox(
-                            color: Colors.green,
-                            child: Text('Colored Box'),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                    const RowWidget(),
-                  ],
-                ),
-              ),
-            ],
-          );
-        },
-      )),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+final List<Widget> listOfWidgets = [
+  const _ListItem(
+    color: Colors.red,
+    iconItem: Icon(
+      Icons.home,
+      size: 50,
+      color: Colors.yellow,
+    ),
+  ),
+  const _ListItem(
+    color: Colors.yellow,
+    iconItem: Icon(
+      Icons.newspaper,
+      size: 50,
+      color: Colors.orange,
+    ),
+  ),
+  const _ListItem(
+    color: Colors.blue,
+    iconItem: Icon(
+      Icons.chat,
+      size: 50,
+      color: Colors.pink,
+    ),
+  ),
+  const _ListItem(
+    color: Colors.green,
+    iconItem: Icon(
+      Icons.umbrella,
+      size: 50,
+      color: Colors.orange,
+    ),
+  ),
+  const _ListItem(
+    color: Colors.purple,
+    iconItem: Icon(
+      Icons.cabin,
+      size: 50,
+      color: Colors.yellowAccent,
+    ),
+  ),
+];
+
+class ListOfWidgets {
+  List<Widget> rowList = <Widget>[];
+
+  List<Widget> fillColumn() {
+    for (var item = 0; item != 19; item++) {
+      for (var item = 0; item < 3; item++) {
+        rowList.add(listOfWidgets[Random().nextInt(4)]);
+      }
+
+      columnList.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: rowList,
+      ));
+      rowList = [];
+    }
+    return columnList;
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final Color color;
+  final Icon iconItem;
+  const _ListItem({required this.color, required this.iconItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: _maxWidth,
+      child: Container(
+        color: color,
+        width: 100,
+        height: 100,
+        child: iconItem,
+      ),
     );
   }
 }
