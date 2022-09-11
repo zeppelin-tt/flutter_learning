d39a1d72c403fc7f67447da45c8a697fab4aa1da
Alexey Karimurzayev <asxtream@gmail.com>
2022-09-11 23:43:04 +0600

    karimurzayev alexey lesson_02: in process

---
+++ lib/participants/karimurzayev_alexey/lessons/lesson_02/karimurzayev_alexey_lesson_2_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class KarimurzayevAlexeyLesson2Page extends StatelessWidget {
+  const KarimurzayevAlexeyLesson2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
